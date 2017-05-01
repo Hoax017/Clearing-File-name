ffa5c847808ba9d1f5c4504cfd22565cf5f8bbcf
Hoax017 <m.aymeric@gmail.com>
2017-05-01 22:38:33 +0200

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+** File-name-Cleaner
+* launch
+` php clear_name.php <directory> `
+EXEMPLE :
+` php .\clear_name.php G:\Film `
---
+++ clear_name.php
@@ -0,0 +1,90 @@
+<?php
+	function my_getfiles($path, $rename = false)
+	{
+		global $list_files;
+		global $root;
+		$path .= '\\';
+
+		echo "\n\n=========================\nCHECKING : $path\n=========================\n\n";
+
+	   	if (is_dir($path)) {
+			if ($dh = opendir($path)) {
+				readdir($dh); // remove .
+				readdir($dh); // remove ..
+				while (($file = readdir($dh)) !== false) {
+					if (filetype($path . $file) == "dir")
+						my_getfiles($path . $file, true);
+					else
+					{
+						if ($rename)
+						{
+							rename("$path$file", "$root\\$file");
+							$list_files[] = array($root.'\\', $file);
+							echo "-> Moved : $file\n";
+
+						}
+						else
+							$list_files[] = array($path, $file);
+					}
+				}
+				closedir($dh);
+			}
+		}
+	}
+
+if ($argc >= 2 && is_dir($argv[1]))
+{
+	$root = $argv[1];
+	$list_files = array();
+
+	$blacklist = array(
+		"/\[.*\]/i",
+		"/20\d{2}/i",
+		"/truefrench/i",
+		"/showfr/i",
+		"/www/i",
+		"/torrent9/i",
+		"/notag/i",
+		"/french/i",
+		"/subforced/i",
+		"/HDRip/i",
+		"/WEBRip/i",
+		"/UNRATED/i",
+		"/BaliBalo/i",
+		"/HuSh/i",
+		"/bdrip/i",
+		"/brrip/i",
+		"/dvdrip/i",
+		"/x264/i",
+		"/extreme/i",
+		"/ biz/i",
+		"/xvid/i",
+		"/lost/i",
+		"/stvfrv/i",
+		"/RERip/i",
+		"/funkky/i",
+		"/svr/i",
+		"/UTT/i",
+		"/AC3/i"
+	);
+
+	my_getfiles($root);
+
+	echo "\n\n=========================\n LIST OF FILES \n=========================\n\n";
+
+	foreach ($list_files as $list_file) {
+		$path_parts = pathinfo($list_file[0].$list_file[1]);
+		$final_name = preg_replace($blacklist, "", $list_file[1]);
+		$final_name = str_replace(".", " ", $final_name);
+		$final_name = str_replace("-", " ", $final_name);
+		$final_name = str_replace(" {$path_parts['extension']}", "", $final_name);
+		$final_name = preg_replace("/[ \t\n\r]+/", " ", $final_name);
+
+		$final_name = trim($final_name);
+		printf("-> %70s => %40s\n", str_replace(".{$path_parts['extension']}", "", $list_file[1]), $final_name);
+		rename($list_file[0].$list_file[1], "{$list_file[0]}$final_name.{$path_parts['extension']}");
+
+	}
+}
+else
+	echo "\nLe chemin est manquant\n\n";
