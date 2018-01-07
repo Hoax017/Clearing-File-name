4c0a1fd189933ab63a64e15ed5d23403f832781a
Hoax017 <m.aymeric@gmail.com>
2018-01-07 17:48:02 +0000

    Remove empty folder ans file inclided in blacklist_file (config.json)

--- src/clear_name.php
+++ src/clear_name.php
@@ -9,53 +9,61 @@
  */
 function My_getfiles($path, $move = false)
 {
-    $list_files = array();
-    global $root;
-    $path .= '/';
+	$list_files = array();
+	global $root, $config;
+	$path .= '/';
 
-    echo "\n\n======================\nCHECKING : $path\n=======================\n\n";
+	echo "\n\n======================\nCHECKING : $path\n=======================\n\n";
 
-    if (is_dir($path)) {
-        if ($dh = opendir($path)) {
-            while (($file = readdir($dh)) !== false) {
-                if ($file == "." || $file == "..") {
-                    continue;
-                }
+	if (is_dir($path)) {
+		if ($dh = opendir($path)) {
+			while (($file = readdir($dh)) !== false) {
+				if ($file == "." || $file == "..") {
+					continue;
+				}
 
-                if (filetype($path . $file) == "dir") {
-                    $list_files = array_merge(
-                        $list_files,
-                        My_getfiles($path . $file, true)
-                    );
-                    continue;
-                }
+				if (filetype($path . $file) == "dir") {
+					$list_files = array_merge(
+						$list_files,
+						My_getfiles($path . $file, true)
+					);
+					continue;
+				}
 
-                if (!preg_match("/(.(avi|mkv|mp4))$/", $file)) {
-                    continue;
-                }
+				if (preg_match('/'.implode('|', $config->blacklist_file) .'/', $file)) {
+					unlink("$path$file");
+					echo "-> Deleted : $file\n";
+				}
+				if (!preg_match($config->match, $file)) {
+					continue;
+				}
 
-                if ($move) {
-                    rename("$path$file", "$root/$file");
-                    $list_files[] = array($root.'/', $file);
-                    echo "-> Moved : $file\n";
+				if ($move) {
+					rename("$path$file", "$root/$file");
+					$list_files[] = array($root.'/', $file);
+					echo "-> Moved : $file\n";
 
-                } else {
-                    $list_files[] = array($path, $file);
-                    echo "$file\n";
-                }
-            }
-            closedir($dh);
-        }
-    } else {
-        echo "$path n'est pas un dossier";
-    }
-    return $list_files;
+				} else {
+					$list_files[] = array($path, $file);
+					echo "$file\n";
+				}
+			}
+			closedir($dh);
+			if ($move && empty(glob($path."*"))) {
+				echo "-> Deleted : $path\n";
+				rmdir($path);
+			}
+		}
+	} else {
+		echo "$path n'est pas un dossier";
+	}
+	return $list_files;
 }
 
 $config = json_decode(file_get_contents(__DIR__."/../config.json"));
 
 if ($argc < 2 || empty($argv[1]) || !is_dir($argv[1])) {
-    $argv[1] = $config->default_folder;
+	$argv[1] = $config->default_folder;
 }
 
 $root = $argv[1];
@@ -68,35 +76,35 @@ $list_files = My_getfiles($root);
 echo "\n\n=========================\n LIST OF FILES \n=========================\n\n";
 
 foreach ($list_files as $list_file) {
-    $path_parts = pathinfo($list_file[0].$list_file[1]);
-    $final_name = preg_replace($blacklist, "", $list_file[1]);
-    $final_name = str_replace(".", " ", $final_name);
-    $final_name = str_replace("-", " ", $final_name);
-    $final_name = str_replace(" {$path_parts['extension']}", "", $final_name);
-    $final_name = preg_replace("/[ \t\n\r]+/", " ", $final_name);
+	$path_parts = pathinfo($list_file[0].$list_file[1]);
+	$final_name = preg_replace($blacklist, "", $list_file[1]);
+	$final_name = str_replace(".", " ", $final_name);
+	$final_name = str_replace("-", " ", $final_name);
+	$final_name = str_replace(" {$path_parts['extension']}", "", $final_name);
+	$final_name = preg_replace("/[ \t\n\r]+/", " ", $final_name);
 
-    // 1X4 to S01E04
-    if (preg_match("/(.+)\s+(\d{1,2})x(\d{1,2})(.*)/i", $final_name, $match)) {
-        $final_name = sprintf("%s S%02dE%02d%s", $match[1], intval($match[2]), intval($match[3]), $match[4]);
-    }
+	// 1X4 to S01E04
+	if (preg_match("/(.+)\s+(\d{1,2})x(\d{1,2})(.*)/i", $final_name, $match)) {
+		$final_name = sprintf("%s S%02dE%02d%s", $match[1], intval($match[2]), intval($match[3]), $match[4]);
+	}
 
-    // TiTLe to Title
-    $final_name = ucwords(strtolower($final_name));
+	// TiTLe to Title
+	$final_name = ucwords(strtolower($final_name));
 
-    // s02e25 to S02E25
-    if (preg_match("/(S\d{2}E\d{2})/i", $final_name, $match)) {
-        $final_name = str_replace($match[1], strtoupper($match[1]), $final_name);
-    }
+	// s02e25 to S02E25
+	if (preg_match("/(S\d{2}E\d{2})/i", $final_name, $match)) {
+		$final_name = str_replace($match[1], strtoupper($match[1]), $final_name);
+	}
 
-    $final_name = trim($final_name);
-    printf(
-        "-> %70s => %40s\n",
-        str_replace(".{$path_parts['extension']}", "", $list_file[1]),
-        $final_name
-    );
-    rename(
-        $list_file[0].$list_file[1],
-        "{$list_file[0]}$final_name.{$path_parts['extension']}"
-    );
+	$final_name = trim($final_name);
+	printf(
+		"-> %70s => %40s\n",
+		str_replace(".{$path_parts['extension']}", "", $list_file[1]),
+		$final_name
+	);
+	rename(
+		$list_file[0].$list_file[1],
+		"{$list_file[0]}$final_name.{$path_parts['extension']}"
+	);
 
 }
--- src/sort_series.php
+++ src/sort_series.php
@@ -4,7 +4,7 @@ $config = json_decode(file_get_contents(__DIR__."/../config.json"));
 
 
 if ($argc < 2 || empty($argv[1]) || !is_dir($argv[1])) {
-    $argv[1] = $config->default_folder;
+	$argv[1] = $config->default_folder;
 }
 
 $path = $argv[1];
@@ -13,30 +13,31 @@ echo "\n\n======================\nSort Showtime : $path\n======================\
 
 
 if (is_dir($path)) {
-    if ($dh = opendir($path)) {
-        while (($file = readdir($dh)) !== false) {
-            if ($file == "."
-                || $file == ".."
-                || !preg_match("/(.*)(S\d{2})E\d{2}/i", $file, $match)
-                || is_dir($file)
-            ) {
-                continue;
-            }
+	if ($dh = opendir($path)) {
+		while (($file = readdir($dh)) !== false) {
+			if ($file == "."
+				|| $file == ".."
+				|| is_dir($file)
+				|| !preg_match($config->match, $file)
+				|| !preg_match("/(.*)(S\d{2})E\d{2}/i", $file, $match)
+			) {
+				continue;
+			}
 
-            $path_parts = pathinfo($file);
-            $serie_name = $match[1];
-            $serie_name = preg_replace("/\s*\-?\s*final(\s*\d+p)?$/i", "$1", $serie_name);
-            $serie_name = preg_replace("/\d+p/i", "", $serie_name);
-            $serie_name = preg_replace("/vostfr/i", "", $serie_name);
-            $serie_name = trim($serie_name);
-            if (!is_dir("$path/$serie_name")) {
-                mkdir("$path/$serie_name");
-            }
-            if (!is_dir("$path/$serie_name/{$match[2]}")) {
-                mkdir("$path/$serie_name/{$match[2]}");
-            }
-            rename("$path/$file", "$path/$serie_name/{$match[2]}/$file");
-            printf("%40s -> %40s\n", $file, "$serie_name/{$match[2]}/$file");
-        }
-    }
+			$path_parts = pathinfo($file);
+			$serie_name = $match[1];
+			$serie_name = preg_replace("/\s*\-?\s*final(\s*\d+p)?$/i", "$1", $serie_name);
+			$serie_name = preg_replace("/\d+p/i", "", $serie_name);
+			$serie_name = preg_replace("/vostfr/i", "", $serie_name);
+			$serie_name = trim($serie_name);
+			if (!is_dir("$path/$serie_name")) {
+				mkdir("$path/$serie_name");
+			}
+			if (!is_dir("$path/$serie_name/{$match[2]}")) {
+				mkdir("$path/$serie_name/{$match[2]}");
+			}
+			rename("$path/$file", "$path/$serie_name/{$match[2]}/$file");
+			printf("%40s -> %40s\n", $file, "$serie_name/{$match[2]}/$file");
+		}
+	}
 }
